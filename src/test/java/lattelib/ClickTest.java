316aff1b5e415c70fa6b498d884f9d2a8cd66e64
charlie <cforster@dalton.org>
2017-10-19 16:37:43 -0400

    update click testing

--- src/test/java/lattelib/ClickTest.java
+++ src/test/java/lattelib/ClickTest.java
@@ -9,18 +9,12 @@ public class ClickTest {
     public static void main(String[] args) {
         WebLatte frame = new WebLatte();
 
-        int i = 0;
+        frame.addButton("mybutton", 200,200);
+        Element e = frame.drawCircle(100, 100, 75, ColorLatte.AliceBlue);
+        WebLatte.makeClickable(e, "mycircle");
+        frame.paint();
 
-//        while(true) {
-            Element e = frame.drawCircle(100, 100+i, 75, ColorLatte.AliceBlue);
-            WebLatte.makeClickable(e, "mycircle");
-            Element e2 = frame.drawRectangle(200,200,75, 75, 0, ColorLatte.Gold);
-            frame.paint();
-//            i++;
-
-//        }
         while(true) {
-            frame.nextLine();
             frame.println(frame.nextClick());
         }
     }
